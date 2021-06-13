b4f65686d7a383a9faa5411c28f7b9877aa9cc95
Hiroki29 <a-hiro29@outlook.jp>
2021-06-14 08:23:14 +0900

    add

--- infer/infer001.py
+++ infer/infer001.py
@@ -1,4 +1,5 @@
 import gc
+import math
 import os
 import random
 import time
@@ -7,11 +8,13 @@ import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
-from torch.utils.data import (DataLoader, Dataset, SequentialSampler)
-from tqdm import tqdm
-from transformers import RobertaConfig, RobertaModel, RobertaTokenizer
+from torch.utils.data import (DataLoader, Dataset, RandomSampler, SequentialSampler)
+from transformers import AutoConfig, AutoModel, AutoTokenizer
 
 
+# =================================================
+# Utilities #
+# =================================================
 def set_seed(seed=42):
     random.seed(seed)
     os.environ["PYTHONHASHSEED"] = str(seed)
@@ -21,6 +24,38 @@ def set_seed(seed=42):
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+
+
+def get_device():
+    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def init_logger(log_file='train.log'):  # コンソールとログファイルの両方に出力
+    from logging import getLogger, INFO, FileHandler, Formatter, StreamHandler
+    logger = getLogger(__name__)
+    logger.setLevel(INFO)  # ログレベルを設定
+    handler1 = StreamHandler()  # ログをコンソール出力するための設定
+    handler1.setFormatter(Formatter("%(message)s"))  # ログメッセージ
+    handler2 = FileHandler(filename=log_file)  # ログのファイル出力先を設定（4）
+    handler2.setFormatter(Formatter("%(message)s"))  # ログメッセージ
+    logger.addHandler(handler1)
+    logger.addHandler(handler2)
+    return logger
+
+
+# =================================================
+# Config #
+# =================================================
+class Config:
+    seed = 42
+    max_len = 300
+    batch_size = 8
+    train_file = '../input/commonlitreadabilityprize/train_folds.csv'
+    scaler = None
+    tokenizer = 'roberta-large'
+    # filename = __file__.split("/")[-1].replace(".py", "")
+    # out_dir = '../out/' + filename
+    # check_dir = out_dir + '/checkpoint'
 
 
 def convert_examples_to_features(data, tokenizer, max_len, is_test=False):
@@ -46,6 +81,7 @@ class DatasetRetriever(Dataset):
     def __init__(self, data, tokenizer, max_len, is_test=False):
         self.data = data
         self.excerpts = self.data.excerpt.values.tolist()
+        self.targets = self.data.target.values.tolist()
         self.tokenizer = tokenizer
         self.is_test = is_test
         self.max_len = max_len
@@ -80,19 +116,12 @@ class DatasetRetriever(Dataset):
 
 
 class CommonLitModel(nn.Module):
-    def __init__(
-            self,
-            model_name,
-            config,
-            multisample_dropout=False,
-            output_hidden_states=False
-    ):
+    def __init__(self, model_name, config, multisample_dropout=False, output_hidden_states=False):
         super(CommonLitModel, self).__init__()
         self.config = config
-        self.roberta = RobertaModel.from_pretrained(
-            model_name,
-            output_hidden_states=output_hidden_states
-        )
+        self.roberta = AutoModel.from_pretrained(model_name,
+                                                 output_hidden_states=output_hidden_states
+                                                 )
         self.layer_norm = nn.LayerNorm(config.hidden_size)
         if multisample_dropout:
             self.dropouts = nn.ModuleList([
@@ -117,13 +146,7 @@ class CommonLitModel(nn.Module):
             module.bias.data.zero_()
             module.weight.data.fill_(1.0)
 
-    def forward(
-            self,
-            input_ids=None,
-            attention_mask=None,
-            token_type_ids=None,
-            labels=None
-    ):
+    def forward(self, input_ids=None, attention_mask=None, token_type_ids=None, labels=None):
         outputs = self.roberta(
             input_ids,
             attention_mask=attention_mask,
@@ -148,36 +171,36 @@ class CommonLitModel(nn.Module):
             logits = logits.view(-1).to(labels.dtype)
             loss = torch.sqrt(loss_fn(logits, labels.view(-1)))
 
-        output = (logits,) + outputs[1:]
+        output = (logits,) + outputs[2:]
         return ((loss,) + output) if loss is not None else output
 
 
 def make_model(model_name, num_labels=1):
-    tokenizer = RobertaTokenizer.from_pretrained(model_name)
-    config = RobertaConfig.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(Config.tokenizer)
+    config = AutoConfig.from_pretrained(model_name)
     config.update({'num_labels': num_labels})
     model = CommonLitModel(model_name, config=config)
     return model, tokenizer
 
 
-def make_loader(
-        data,
-        tokenizer,
-        max_len,
-        batch_size,
-):
-    test_dataset = DatasetRetriever(data, tokenizer, max_len, is_test=True)
-    test_sampler = SequentialSampler(test_dataset)
-    test_loader = DataLoader(
-        test_dataset,
-        batch_size=batch_size // 2,
-        sampler=test_sampler,
-        pin_memory=False,
-        drop_last=False,
-        num_workers=0
+def make_loader(data, tokenizer, max_len, batch_size, fold=0):
+    train_set, valid_set = data[data['kfold'] != fold], data[data['kfold'] == fold]
+    train_dataset = DatasetRetriever(train_set, tokenizer, max_len)
+    valid_dataset = DatasetRetriever(valid_set, tokenizer, max_len)
+
+    train_sampler = RandomSampler(train_dataset)
+    train_loader = DataLoader(
+        train_dataset, batch_size=batch_size, sampler=train_sampler,
+        pin_memory=True, drop_last=False, num_workers=4
     )
 
-    return test_loader
+    valid_sampler = SequentialSampler(valid_dataset)
+    valid_loader = DataLoader(
+        valid_dataset, batch_size=batch_size, sampler=valid_sampler,
+        pin_memory=True, drop_last=False, num_workers=4
+    )
+
+    return train_loader, valid_loader
 
 
 class Evaluator:
@@ -217,19 +240,18 @@ class Evaluator:
 
 
 def config(fold, model_name, load_model_path):
-    max_len = 300
-    batch_size = 16
-    test = pd.read_csv('../input/commonlitreadabilityprize/train_folds.csv')
+    max_len = Config.max_len
+    batch_size = Config.batch_size
+
+    test = pd.read_csv(Config.train_file)
     model, tokenizer = make_model(
-        model_name=model_name,
-        num_labels=1
-    )
+        model_name=Config.model_name, num_labels=1)
+
     model.load_state_dict(
         torch.load(f'{load_model_path}/model{fold}.bin')
     )
-    test_loader = make_loader(
-        test, tokenizer, max_len=max_len,
-        batch_size=batch_size
+    train_loader, valid_loader = make_loader(
+        test, tokenizer, max_len=max_len, batch_size=batch_size, fold=fold
     )
 
     if torch.cuda.device_count() >= 1:
@@ -241,16 +263,16 @@ def config(fold, model_name, load_model_path):
     else:
         raise ValueError('CPU training is not supported')
 
-    # scaler = torch.cuda.amp.GradScaler()
-    scaler = None
+    scaler = Config.scaler
+
+
     return (
-        model, tokenizer,
-        test_loader, scaler
+        model, tokenizer, valid_loader, scaler
     )
 
 
 def run(fold=0, model_name=None, load_model_path=None):
-    model, tokenizer, test_loader, scaler = config(fold, model_name, load_model_path)
+    model, tokenizer, valid_loader, scaler = config(fold, model_name, load_model_path)
     evaluator = Evaluator(model, scaler)
 
     test_time_list = []
@@ -258,33 +280,29 @@ def run(fold=0, model_name=None, load_model_path=None):
     torch.cuda.synchronize()
     tic1 = time.time()
 
-    preds = evaluator.evaluate(test_loader, tokenizer)
+    preds = evaluator.evaluate(valid_loader, tokenizer)
 
     torch.cuda.synchronize()
     tic2 = time.time()
     test_time_list.append(tic2 - tic1)
 
-    del model, tokenizer, test_loader, scaler
+    torch.cuda.empty_cache()
+    del model, tokenizer, valid_loader, scaler
     gc.collect()
     torch.cuda.empty_cache()
-
     return preds
 
 
 if __name__ == '__main__':
-    set_seed(42)
-    pred_df1 = pd.DataFrame()
-    pred_df2 = pd.DataFrame()
-    pred_df3 = pd.DataFrame()
-    for fold in tqdm(range(5)):
-        pred_df1[f'fold{fold}'] = run(fold, 'roberta-large',
-                                      '../out/exp010_RoBERTa_large_FITv2/checkpoint/')
-        # pred_df2[f'fold{fold + 5}'] = run(fold, '../input/robertalarge/',
-        #                                   '../input/roberta-large-itptfit/')
-        # pred_df3[f'fold{fold + 10}'] = run(fold, '../input/robertalarge/',
-        #                                    '../input/commonlit-roberta-large-ii/')
-    test = pd.read_csv('../input/commonlitreadabilityprize/train.csv')
-    print(pred_df1)
-    pred_df1['target'] = pred_df1.mean(axis=1).values.tolist()
-    rmse = np.sqrt(np.mean((test['target'] - pred_df1['target']) ** 2))
-    print(rmse)
+    set_seed(Config.seed)
+    result_list = []
+    for fold in range(5):
+        print('----')
+        print(f'FOLD: {fold}')
+        preds = run(fold, 'roberta-large','../out/exp010_RoBERTa_large_FITv2/checkpoint/')
+        print(preds)
+        # result_list.append(result_dict)
+        print('----')
+
+    # logger = init_logger(log_file=Config.out_dir + "/train.log")
+    # logger.info(result_list)
